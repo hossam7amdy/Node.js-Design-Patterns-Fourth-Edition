4dcea143f03084375fcfbc2ee9803fde1a581d4e
Hossam Hamdy <hossamhamdy117@gmail.com>
2026-04-15 21:02:37 +0200

    refactor: migrate test examples from JavaScript to TypeScript

---
+++ 10-testing/05-test-concurrency/example.test.ts
@@ -0,0 +1,13 @@
+import { test } from 'node:test'
+import { setTimeout } from 'node:timers'
+import { setTimeout as delay } from 'node:timers/promises'
+
+test('Top Level Test', { concurrency: true }, t => {
+  t.test('SubTest 1', async () => {
+    await delay(1000)
+  })
+
+  t.test('SubTest 2', (t, done) => {
+    setTimeout(done, 1000)
+  })
+})
