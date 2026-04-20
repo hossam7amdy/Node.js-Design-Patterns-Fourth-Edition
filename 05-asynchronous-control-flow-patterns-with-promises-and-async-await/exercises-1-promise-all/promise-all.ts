d8bd229fbbcaa4cebf4b71002b3b4714c9bc0623
Hossam Hamdy <hossamhamdy117@gmail.com>
2026-04-20 10:52:52 +0200

    refactor: simplify promiseAll for better readability

--- 05-asynchronous-control-flow-patterns-with-promises-and-async-await/exercises-1-promise-all/promise-all.ts
+++ 05-asynchronous-control-flow-patterns-with-promises-and-async-await/exercises-1-promise-all/promise-all.ts
@@ -1,37 +1,26 @@
-function isNotIterable(obj: unknown): boolean {
-  // biome-ignore lint/suspicious/noExplicitAny: accepts any random type
-  return !obj || typeof (obj as any)[Symbol.iterator] !== 'function'
-}
-
 export function promiseAll<T>(
   promises: Iterable<T | PromiseLike<T>>
 ): Promise<T[]> {
   return new Promise((resolve, reject) => {
-    if (isNotIterable(promises)) {
-      return reject(new TypeError(`object ${promises} is not iterable`))
-    }
-
-    let count = 0
+    let totalItems = 0
+    let completedCount = 0
     const results: T[] = []
-    const promisesArr = Array.from(promises)
 
-    if (promisesArr.length === 0) {
-      return resolve(results)
-    }
-
-    for (let i = 0; i < promisesArr.length; i++) {
-      Promise.resolve(promisesArr[i]).then(
-        r => {
-          count++
-          results[i] = r as T
-          if (count === promisesArr.length) {
+    for (const promise of promises) {
+      const index = totalItems++
+      Promise.resolve(promise)
+        .then(value => {
+          completedCount++
+          results[index] = value
+          if (completedCount === totalItems) {
             resolve(results)
           }
-        },
-        e => {
-          reject(e)
-        }
-      )
+        })
+        .catch(reject)
+    }
+
+    if (totalItems === 0) {
+      resolve(results)
     }
   })
 }
